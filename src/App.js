8e26a9e095e45557d8f9b1ddeb543f20d9d9071c
InshaNazeer <insha@xelpmoc.in>
2018-02-08 10:21:25 +0530

    done react-practice-3

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import MultiDropdown from "./MultiDropdown.js";
 import "./App.css";
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      country:null,
+      city:null
+    };
+  }
   onChange(val) {
-    console.log(val);
+    if(val.country){
+      console.log({"selectedValue1":val.country.value,"selectedValue2":null})
+      this.setState({country:val.country.value});
+      }
+    if(val.city){
+      console.log({"selectedValue1":this.state.country,"selectedValue2":val.city.value})
+      this.setState({city:val.city.value});
+      }
   }
   render() {
     return (
       <div className="App">
-        <MultiDropdown onChange={val => this.onChange(val)} />
+        <MultiDropdown onChange={val => this.onChange(val)} country={this.state.country} city={this.state.city} />
       </div>
     );
   }
--- src/MultiDropdown.js
+++ src/MultiDropdown.js
@@ -3,20 +3,60 @@ import PropTypes from "prop-types";
 import * as countriesCities from "countries-cities";
 import "./MultiDropdown.css";
 import _ from "lodash";
-
-// HINT User CustomDropdown.js here
+import Select from "react-select";
+import CustomDropdown from "./CustomDropdown.js";
 
 export default class MultiDropdown extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      country:null,
+      city:null,
+    };
+    this.countryOptions = _.map(countriesCities.getCountries(), country => {
+      return {
+        label: country,
+        value: country
+      };
+    });
   }
-
-  onChange() {}
-
+  onChange(value) {
+    if(this.props.onChange){
+    this.props.onChange(value);
+    }
+  }
   render() {
-    return <div className="MultiDropdown" />;
+    let citiesForCountry;
+    if (this.props.country) {
+      citiesForCountry=_.map(countriesCities.getCities(this.props.country),city=>{
+        return{
+          label:city,
+          value:city
+        };
+      });
+    }
+    return (
+       <div className="MultiDropdown">
+       <div className="country">
+         Country
+           <CustomDropdown 
+           options={this.countryOptions}
+           value={this.props.country}
+           onChange={country => this.onChange({ country })}/>
+        </div>
+        <div className="city">
+           City
+          <CustomDropdown  
+           options={citiesForCountry}
+           value={this.props.city} 
+           onChange={city => this.onChange({ city })}/>
+        </div>
+    </div>);
   }
 }
 
-MultiDropdown.propTypes = {};
+MultiDropdown.propTypes = {
+  onChange:PropTypes.func,
+  city:PropTypes.string,
+  country:PropTypes.string,
+};
