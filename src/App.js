6e4b40025e8596247eba6c4d9e7a2cb2e508b1fd
InshaNazeer <insha@xelpmoc.in>
2018-02-09 11:15:24 +0530

    removeUnexpectedCode

--- src/App.js
+++ src/App.js
@@ -3,13 +3,6 @@ import logo from "./logo.svg";
 import MultiDropdown from "./MultiDropdown.js";
 import "./App.css";
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      country:null,
-      city:null
-    };
-  }
   onChange(val) {
     console.log(val)
   }
