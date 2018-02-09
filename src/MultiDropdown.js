1ee47ec068854f0ea81d166cf317e34858e960d0
Truptimayee031 <shruti@xelpmoc.in>
2018-02-09 11:03:18 +0530

    getting country and city in console

--- src/MultiDropdown.js
+++ src/MultiDropdown.js
@@ -3,20 +3,78 @@ import PropTypes from "prop-types";
 import * as countriesCities from "countries-cities";
 import "./MultiDropdown.css";
 import _ from "lodash";
-
-// HINT User CustomDropdown.js here
+import Select from "react-select";
+import CustomDropdown from "./CustomDropdown.js";
 
 export default class MultiDropdown extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      country: "",
+      city: ""
+    };
+    this.countryList = _.map(countriesCities.getCountries(), country => {
+      return {
+        label: country,
+        value: country
+      };
+    });
   }
-
-  onChange() {}
-
+  getCountryList(value) {
+    if (value.country) {
+      this.setState({ country: value.country.value, city: "" }, () => {
+        this.props.onChange(this.state);
+      });
+    }
+  }
+  getCityList(value) {
+    if (value.city) {
+      this.setState({ city: value.city.value }, () => {
+        this.props.onChange(this.state);
+      });
+    }
+  }
+  onChange(value) {
+    if (value.country) {
+      this.getCountryList(value);
+    }
+    if (value.city && this.state.country) {
+      this.getCityList(value);
+    }
+  }
   render() {
-    return <div className="MultiDropdown" />;
+    let cityList;
+    if (this.state.country) {
+      cityList = _.map(countriesCities.getCities(this.state.country), city => {
+        return {
+          label: city,
+          value: city
+        };
+      });
+    }
+    return (
+      <div className="MultiDropdown">
+        <div>
+          <CustomDropdown
+            placeholder="Select Country"
+            options={this.countryList}
+            value={this.state.country}
+            onChange={country => this.onChange({ country })}
+          />
+        </div>
+        <div>
+          <CustomDropdown
+            placeholder="Select City"
+            options={cityList}
+            value={this.state.city}
+            onChange={city => this.onChange({ city })}
+          />
+        </div>
+      </div>
+    );
   }
 }
 
-MultiDropdown.propTypes = {};
+MultiDropdown.propTypes = {
+  onChange: PropTypes.func
+};
